f1f6a349e4a63dde15644c589b0dcade4df6718c
microblur <microblur@sina.cn>
2022-03-30 23:19:24 +1300

    add some assertion

--- cypress/integration/OrderingBook/orderingBook.js
+++ cypress/integration/OrderingBook/orderingBook.js
@@ -10,9 +10,10 @@ Given('The user launch chrome broswer with {string}', (url) => {
 
 When('The user select category as {string}',(category)=>{
     if (category=="Fiction")
-        cy.get('[id="radioselect1"]').click()
+       // cy.get('[id="radioselect1"]').check().should('be.checked');
+       cy.get('[value=Fiction]').should('be.enabled').check().should('be.checked');
     else{
-        cy.get('[value="Drama"]').click()
+        cy.get('[value=Drama]').should('be.enabled').check().should('be.checked');
     }
 })
 And('The user select {string} from the dropdown list',(name)=>{
@@ -22,23 +23,24 @@ And('The user select {string} from the dropdown list',(name)=>{
 
 And('The user enters the units as {string}',(units)=>{
     bUnits = units;
-    cy.get('[name="units"]').should('be.visible').should('be.enabled').type(units);
+    cy.get('[name="units"]').should('be.visible').should('be.enabled').type(units).should('have.value',units);
 })
 
 And('The user enters the price as {string}',(price)=>{
     bPrice = price;
-    cy.get('[name="price"]').should('be.visible').should('be.enabled').type(price);
+    cy.get('[name="price"]').should('be.visible').should('be.enabled').type(price).should('have.value',price);
 })
 And('The user select discount checkbox',()=>{
-    cy.get('[name="discount"]').click();
+    cy.get('[name="discount"]').check().should('be.checked');
 })
 And ('The user enters {string} in discount',(discount)=>{
     bDiscount = discount;
-    cy.get('[name="discountvalue"]').should('be.visible').should('be.enabled').type(discount);
+    cy.get('[name="discountvalue"]').should('be.visible').should('be.enabled').type(discount).should('have.value',discount);
 }) 
 
 And('The user click the submit button',()=>{
     cy.get('[name="submit"]').click();
+   
     cy.wait(2000);
 
 })
@@ -48,6 +50,7 @@ Then('The transaction record will be generated', ()=>{
     if(bDiscount!=0){
         fAmount = amount*(100-bDiscount)/100;
     }
+
     cy.get('tr').eq(0).should('contain',bName);
     cy.get('tr').eq(0).should('contain',bPrice);
     cy.get('tr').eq(0).should('contain',amount);
